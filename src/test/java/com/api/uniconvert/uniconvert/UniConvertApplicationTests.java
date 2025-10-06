09d4a9103b326286255ad7a8049301945227d95f
aunt-htoo-aung <ahnthtooaung5@gmail.com>
2025-10-06 20:54:37 +0630

    Initial commit for UniConvert Api

---
+++ src/test/java/com/api/uniconvert/uniconvert/UniConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.uniconvert.uniconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
